4f037c5e2231b772c125c42e31cb3458ba8cee3d
Anton Khludnev <149969298+sweetiedice@users.noreply.github.com>
2023-12-01 14:22:56 +0300

    Add files via upload

---
+++ CRM/LoginPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CRM
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonLoginClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Авторизация в приложении находится в разработке!", "Регистрация", MessageBoxButton.OK, MessageBoxImage.Question);
+        }
+
+        private void hyperlinkLoginGuestClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void hyperlinkRegistrationClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Регистрация в приложении находится в разработке!", "Регистрация", MessageBoxButton.OK, MessageBoxImage.Question);
+        }
+    }
+}
